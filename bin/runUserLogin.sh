32d3b563c8fb48f15b24b6dff080fef134f66052
hodkashtan <hod.kashtan@jivesoftware.com>
2018-01-07 10:41:31 +0200

    minor

--- bin/runUserLogin.sh
+++ bin/runUserLogin.sh
@@ -5,7 +5,7 @@ git pull
 rm -Rf Summary.csv
 NOW=User_Login_$(date +"%m-%d-%y"--"%T")
 mkdir $NOW
-./jmeter.sh -n -t UserRegistration.jmx -l scriptresults.jtl
+./jmeter.sh -n -t UserLogin.jmx -l scriptresults.jtl
 ./jmeter -g Summary.csv -o $NOW
 git add -A
 git commit -m "added $NOW results"
