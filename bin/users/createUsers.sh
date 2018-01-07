f891673ed6dd846dff16cf0630a86ec77c6e7c25
hodkashtan <hod.kashtan@jivesoftware.com>
2018-01-07 10:01:13 +0200

    add users file

---
+++ bin/users/createUsers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm users.csv
+rm users1.csv
+for filename in *.json; do
+        echo -n "$filename" | cut -c 6- | rev | cut -c 6- | rev >> "users1.csv"
+        #echo -n ",">> "users.csv"
+        #AccessToken=$(grep -Eo '.accessToken.{0,143}' "$filename" | cut -c 16-)
+        #echo -n "$AccessToken" >> "users.csv"
+        echo "" >> "users1.csv"
+done
+sed '/^$/d' users1.csv > users.csv
