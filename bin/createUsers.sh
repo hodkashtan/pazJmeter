e8c57acdb1d4f3b7cdbe9178f14e9338bb225db4
Cloud User <centos@ip-172-20-59-232.eu-west-1.compute.internal>
2018-01-07 12:13:03 +0000

    minor

--- bin/createUsers.sh
+++ bin/createUsers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm users.csv
-for filename in *; do
+for filename in ./users/*; do
         echo -n "$filename" | cut -c 6- >> "users1.csv"
         #echo -n ",">> "users.csv"
         #AccessToken=$(grep -Eo '.accessToken.{0,143}' "$filename" | cut -c 16-)
