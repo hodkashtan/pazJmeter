0aafb0489cbdfd06f63f6609ed29e99d71a6e179
Cloud User <centos@ip-172-20-59-232.eu-west-1.compute.internal>
2018-01-08 13:15:04 +0000

    added User_Registration_01-08-18--13:14:21 results

--- bin/createUsers.sh
+++ bin/createUsers.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-rm users.csv
+rm /users/users.csv
 for filename in ./users/*; do
         echo -n "$filename" | cut -c 6- >> "users1.csv"
         #echo -n ",">> "users.csv"
         #AccessToken=$(grep -Eo '.accessToken.{0,143}' "$filename" | cut -c 16-)
         #echo -n "$AccessToken" >> "users.csv"
-        echo "" >> "users1.csv"
+        echo "" >> "./users/users1.csv"
 done
-sed '/^$/d' users1.csv > users.csv
+sed '/^$/d' users1.csv > ./users/users.csv
 rm users1.csv
