e994b2263115246888269200b9fca79a8ae3d279
hodkashtan <root@ip-172-20-59-232.eu-west-1.compute.internal>
2018-01-09 13:39:09 +0000

    changed run script

--- bin/runUserRegistration.sh
+++ bin/runUserRegistration.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 git pull
+rm -Rf target
 rm -Rf Summary.csv
 NOW=User_Registration_$(date +"%m-%d-%y"--"%T")
 mkdir $NOW
 ./jmeter.sh -n -t UserRegistration.jmx -l scriptresults.jtl
 ./jmeter -g Summary.csv -o $NOW
 ./users/createUsers.sh
+mkdir target
+cp -r $NOW/* target/*
 git add -A
 git commit -m "added $NOW results"
 git push
