e085048e0fd01a80635281e22cab1153c81a370d
Cloud User <centos@ip-172-20-59-232.eu-west-1.compute.internal>
2018-01-10 11:34:00 +0000

    a

--- bin/runUserRegistration.sh
+++ bin/runUserRegistration.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+git checkout master
 git pull
 rm -Rf target
 rm -Rf Summary.csv
@@ -7,9 +7,8 @@ NOW=User_Registration_$(date +"%m-%d-%y"--"%T")
 mkdir $NOW
 ./jmeter.sh -n -t UserRegistration.jmx -l scriptresults.jtl
 ./jmeter -g Summary.csv -o $NOW
-./users/createUsers.sh
+./createUsers.sh
 mkdir target
 cp -r $NOW/* target/*
 git add -A
 git commit -m "added $NOW results"
-git push
