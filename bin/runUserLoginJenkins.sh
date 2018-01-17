4bd6d668c470b8748b2cd977f23dc8e33b2f3487
hodkashtan <hod.kashtan@jivesoftware.com>
2018-01-17 02:17:47 +0200

    minor

--- bin/runUserLoginJenkins.sh
+++ bin/runUserLoginJenkins.sh
@@ -4,8 +4,7 @@ git pull
 rm -Rf Summary.csv
 NOW=User_Login_$(date +"%m-%d-%y"--"%T")
 mkdir $NOW
-./jmeter.sh -n -t UserLogin.jmx -l scriptresults.jtl -JNUM_OF_USERS=10
+./jmeter.sh -n -t UserLogin.jmx -l scriptresults.jtl -JNUM_OF_USERS=$NUM_OF_USERS
 ./jmeter -g Summary.csv -o $NOW
 git add -A
 git commit -m "added $NOW results"
-git push
