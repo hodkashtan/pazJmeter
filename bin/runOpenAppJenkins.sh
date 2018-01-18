6ec939f6f0762011617400eff8bd6851c9c94b1a
Cloud User <centos@ip-172-20-59-232.eu-west-1.compute.internal>
2018-01-18 11:58:30 +0000

    added Open_app_01-18-18--11:58:20 results

---
+++ bin/runOpenAppJenkins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git pull
+rm -Rf Summary.csv
+NOW=Open_app_$(date +"%m-%d-%y"--"%T")
+mkdir $NOW
+./jmeter.sh -n -t openApp.jmx -l scriptresults.jtl -JNUM_OF_USERS=$NUM_OF_USERS
+./jmeter -g Summary.csv -o $NOW
+git add -A
+git commit -m "added $NOW results"
+git push
