353c1d120e6b96bf5365b9e6b844105310e4b5f7
Shay Toledano <shay.toledano@jivesoftware.com>
2018-01-21 19:03:38 +0200

    added test history app

---
+++ bin/runHistory.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git pull
+rm -Rf Summary.csv
+NOW=Open_app_$(date +"%m-%d-%y"--"%T")
+mkdir $NOW
+./jmeter.sh -n -t runHistory.jmx -l scriptresults.jtl -JNUM_OF_USERS=10
+./jmeter -g Summary.csv -o $NOW
+git add -A
+git commit -m "added $NOW results"
+git push
