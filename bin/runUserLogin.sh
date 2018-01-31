42763ad5784624912662b600290f3a9580079283
Shay Toledano <shay.toledano@jivesoftware.com>
2018-01-31 17:22:08 +0200

    minor

--- bin/runUserLogin.sh
+++ bin/runUserLogin.sh
@@ -1,25 +1,11 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<jmeterTestPlan version="1.2" properties="3.2" jmeter="3.3 r1808647">
-  <hashTree>
-    <HTTPSamplerProxy guiclass="HttpTestSampleGui" testclass="HTTPSamplerProxy" testname="GenerateVerifiactionCode" enabled="true">
-      <elementProp name="HTTPsampler.Arguments" elementType="Arguments" guiclass="HTTPArgumentsPanel" testclass="Arguments" testname="User Defined Variables" enabled="true">
-        <collectionProp name="Arguments.arguments"/>
-      </elementProp>
-      <stringProp name="HTTPSampler.domain">ltdapi.paz.co.il</stringProp>
-      <stringProp name="HTTPSampler.port">443</stringProp>
-      <stringProp name="HTTPSampler.protocol">https</stringProp>
-      <stringProp name="HTTPSampler.contentEncoding"></stringProp>
-      <stringProp name="HTTPSampler.path">/api/users/generateVerificationCode?mobilePhone=${phoneNumber}&amp;sms=false</stringProp>
-      <stringProp name="HTTPSampler.method">GET</stringProp>
-      <boolProp name="HTTPSampler.follow_redirects">true</boolProp>
-      <boolProp name="HTTPSampler.auto_redirects">false</boolProp>
-      <boolProp name="HTTPSampler.use_keepalive">true</boolProp>
-      <boolProp name="HTTPSampler.DO_MULTIPART_POST">false</boolProp>
-      <stringProp name="HTTPSampler.embedded_url_re"></stringProp>
-      <stringProp name="HTTPSampler.connect_timeout"></stringProp>
-      <stringProp name="HTTPSampler.response_timeout"></stringProp>
-      <stringProp name="TestPlan.comments">GenerateVerifiactionCode</stringProp>
-    </HTTPSamplerProxy>
-    <hashTree/>
-  </hashTree>
-</jmeterTestPlan>
+#!/bin/bash
+
+git pull
+rm -Rf Summary.csv
+NOW=Run_UserLogin_$(date +"%m-%d-%y"--"%T")
+mkdir $NOW
+./jmeter.sh -n -t UserLogin.jmx -l scriptresults.jtl -JNUM_OF_USERS=10
+./jmeter -g Summary.csv -o $NOW
+git add -A
+git commit -m "added $NOW results"
+git push
