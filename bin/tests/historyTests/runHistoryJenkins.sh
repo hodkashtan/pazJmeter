96b44be6f9afea4f3e77a946735cf1069fbda180
Shay Toledano <shay.toledano@jivesoftware.com>
2018-02-01 09:11:55 +0200

    minor

--- bin/tests/historyTests/runHistoryJenkins.sh
+++ bin/tests/historyTests/runHistoryJenkins.sh
@@ -6,9 +6,11 @@ rm -Rf target
 rm -Rf HistorySummary.csv
 NOW=Run_History_$(date +"%m-%d-%y"--"%T")
 mkdir $NOW
-$WORKSPACE/bin/jmeter.sh -n -t runHistory.jmx -l $WORKSPACE/bin/scriptresults.jtl -JNUM_OF_USERS=$NUM_OF_USERS
-$WORKSPACE/bin/jmeter -g HistorySummary.csv -o $NOW
-$WORKSPACE/bin/createUsers.sh
+chmod +x $WORKSPACE/bin/jmeter.sh 
+-n -t runHistory.jmx -l chmod +x $WORKSPACE/bin/scriptresults.jtl -JNUM_OF_USERS=$NUM_OF_USERS
+chmod +x $WORKSPACE/bin/jmeter 
+-g HistorySummary.csv -o $NOW
+chmod +x $WORKSPACE/bin/createUsers.sh
 mkdir target
 cp -r $NOW/* target/
 git add -A
