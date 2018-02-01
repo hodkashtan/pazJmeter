73708a603592276db44e8061a8913ef73a3c2e85
Shay Toledano <shay.toledano@jivesoftware.com>
2018-02-01 08:33:06 +0200

    test for run in remote folder

--- bin/tests/historyTests/runHistoryJenkins.sh
+++ bin/tests/historyTests/runHistoryJenkins.sh
@@ -2,12 +2,12 @@
 git checkout master
 git pull
 rm -Rf target
-rm -Rf Summary.csv
+rm -Rf HistorySummary.csv
 NOW=Run_History_$(date +"%m-%d-%y"--"%T")
 mkdir $NOW
-./jmeter.sh -n -t runHistory.jmx -l scriptresults.jtl -JNUM_OF_USERS=$NUM_OF_USERS
-./jmeter -g Summary.csv -o $NOW
-./createUsers.sh
+/bin/jmeter.sh -n -t runHistory.jmx -l /bin/scriptresults.jtl -JNUM_OF_USERS=$NUM_OF_USERS
+/bin/jmeter -g HistorySummary.csv -o $NOW
+/bin/createUsers.sh
 mkdir target
 cp -r $NOW/* target/
 git add -A
