a9c41281fa532210773a49568a5453115d5ddaa3
Shay Toledano <shay.toledano@jivesoftware.com>
2018-02-01 09:02:15 +0200

    minor

--- bin/tests/historyTests/runHistoryJenkins.sh
+++ bin/tests/historyTests/runHistoryJenkins.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+WORKSPACE='/home/centos/pazJmeter'
 git checkout master
 git pull
 rm -Rf target
 rm -Rf HistorySummary.csv
 NOW=Run_History_$(date +"%m-%d-%y"--"%T")
 mkdir $NOW
-/bin/jmeter.sh -n -t runHistory.jmx -l /bin/scriptresults.jtl -JNUM_OF_USERS=$NUM_OF_USERS
-/bin/jmeter -g HistorySummary.csv -o $NOW
-/bin/createUsers.sh
+$WORKSPACE/bin/jmeter.sh -n -t runHistory.jmx -l $WORKSPACE/bin/scriptresults.jtl -JNUM_OF_USERS=$NUM_OF_USERS
+$WORKSPACE/bin/jmeter -g HistorySummary.csv -o $NOW
+$WORKSPACE/bin/createUsers.sh
 mkdir target
 cp -r $NOW/* target/
 git add -A
