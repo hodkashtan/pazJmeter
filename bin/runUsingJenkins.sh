f9a12f392dc2a94c0b3e55f942f1afd60404d0a8
hodkashtan <hod.kashtan@automat-it.com>
2018-02-04 01:00:00 +0200

    modified bash script to be generic

---
+++ bin/runUsingJenkins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+git checkout master
+git pull
+rm -Rf target
+rm -Rf Summary.csv
+NOW="${JOB_NAME}_$(date +"%m-%d-%y"--"%T")"
+mkdir $NOW
+touch ./runLogs/$NOW.txt
+./jmeter.sh -n -t $JOB_NAME.jmx -l scriptresults.jtl -JNUM_OF_USERS=$NUM_OF_USERS -j ./runLogs/$NOW.txt
+./jmeter -g Summary.csv -o $NOW
+echo 'Jmeter logs is available here:\n'
+link="http://34.250.19.38:8080/job/${JOB_NAME}/ws/bin/runLogs/${NOW}.txt"
+echo $link
+mkdir target
+cp -r $NOW/* target/
+rm -Rf $NOW/
+git add -A
+git commit -m "added $NOW results"
