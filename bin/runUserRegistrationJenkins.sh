e69a987eac3dd6a8685dcda352698e358fb05338
hodkashtan <hod.kashtan@automat-it.com>
2018-02-04 00:40:51 +0200

    minor

--- bin/runUserRegistrationJenkins.sh
+++ bin/runUserRegistrationJenkins.sh
@@ -9,7 +9,7 @@ touch ./runLogs/$NOW.txt
 ./jmeter.sh -n -t UserRegistration.jmx -l scriptresults.jtl -JNUM_OF_USERS=$NUM_OF_USERS -j ./runLogs/$NOW.txt
 ./jmeter -g Summary.csv -o $NOW
 echo 'Jmeter logs is available here:\n'
-link="http://34.250.19.38:8080/job/Run%20user%20registration/ws/bin/runLogs/${NOW}.txt"
+link="http://34.250.19.38:8080/job/${JOB_NAME}/ws/bin/runLogs/${NOW}.txt"
 echo $link
 mkdir target
 cp -r $NOW/* target/
