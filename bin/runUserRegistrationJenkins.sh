4b9cb35346575a5229722d8d1a0fe778f5f0c0bc
hodkashtan <hod.kashtan@automat-it.com>
2018-02-03 03:09:25 +0200

    added logging to userRegistration

--- bin/runUserRegistrationJenkins.sh
+++ bin/runUserRegistrationJenkins.sh
@@ -5,8 +5,8 @@ rm -Rf target
 rm -Rf Summary.csv
 NOW=User_Registration_$(date +"%m-%d-%y"--"%T")
 mkdir $NOW
-echo "LOG_FILE_NAME=runLogs/$NOW.txt"
-./jmeter.sh -n -t UserRegistration.jmx -l scriptresults.jtl -JNUM_OF_USERS=$NUM_OF_USERS -JLOG_FILE_NAME=runLogs/$NOW.txt
+touch ./runLogs/$NOW.txt
+./jmeter.sh -n -t UserRegistration.jmx -l scriptresults.jtl -JNUM_OF_USERS=$NUM_OF_USERS -j ./runLogs/$NOW.txt
 ./jmeter -g Summary.csv -o $NOW
 ./createUsers.sh
 mkdir target
