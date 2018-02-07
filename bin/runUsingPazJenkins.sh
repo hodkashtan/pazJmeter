f68da073536e07d9f550e4f2200e299325ad5f91
Shay Toledano <shay.toledano@jivesoftware.com>
2018-02-07 13:32:08 +0200

    adding the logs for all the jobs. adding the ramp up and loop to jenkins

--- bin/runUsingPazJenkins.sh
+++ bin/runUsingPazJenkins.sh
@@ -6,7 +6,7 @@ rm -Rf Summary.csv
 NOW="${JOB_NAME}_$(date +"%m-%d-%y"--"%T")"
 mkdir $NOW
 touch ./runLogs/$NOW.txt
-./jmeter.sh -n -t $JOB_NAME.jmx -l scriptresults.jtl -JNUM_OF_USERS=$NUM_OF_USERS -j ./runLogs/$NOW.txt
+./jmeter.sh -n -t $JOB_NAME.jmx -l scriptresults.jtl -JNUM_OF_USERS=$NUM_OF_USERS -JRAMP_UP_PERIOD=$RAMP_UP_PERIOD -JLOOP_COUNT=$LOOP_COUNT -j ./runLogs/$NOW.txt
 ./jmeter -g Summary.csv -o $NOW
 echo 'Jmeter logs is available here:\n'
 link="https://172.30.39.139:8080/job/${JOB_NAME}/ws/bin/runLogs/${NOW}.txt"
